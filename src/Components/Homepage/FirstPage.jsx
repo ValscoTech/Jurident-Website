ac8b2e46592020bc8cd362e1bd2069573e2d9d7f
SatyamPundir <satyampundir03@gmail.com>
2023-11-10 19:21:10 +0530

    made hero1 responsive for smaller-devices

--- src/Components/Homepage/FirstPage.jsx
+++ src/Components/Homepage/FirstPage.jsx
@@ -16,14 +16,14 @@ function FirstPage() {
   }, [screenSize]);
   if (screenSize > 1200) {
     return (
-      <div className=" h-screen">
+      <div className=" relative h-screen">
         <img
           src={BG}
           alt="bg"
           className="absolute ml-[546px]  w-[732px] h-[800px] pb-16"
         />
         <div className="absolute h-[100%] w-[100%]">
-          <h1 className=" w-[406px] h-[74px] text-[100px] font-poppins ml-[163px] mt-[173px]  font-black leading-[135px]">
+          <h1 className=" w-[406px] h-[74px] text-[100px] font-poppins ml-[163px] mt-[173px] p-3  font-black leading-[135px]">
             Jurident
           </h1>
           <h2 className="mt-[79px] ml-[158px] w-[773px] h-[164px] font-poppins font-bold text-[70px] leading-[108px]">
@@ -55,32 +55,31 @@ function FirstPage() {
     );
   } else {
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="flex flex-col items-center text-center justify-center">
-          <h1 className="text-[90px] font-poppins font-black leading-[135px]">
-            Jurident
-          </h1>
-          <h2 className="text-[30px] font-poppins  font-black leading-[135px]">
-            <span className=" text-blue-500">Empowring</span> Justice,{" "}
-            <span className="text-blue-500">Protecting</span> Rights
-          </h2>
-          <div className="flex items-center justify-center  space-x-2">
-            <div className="flex items-center gap-8 text-xl h-16 border-2 border-gray-800 rounded-md">
-              <SlLocationPin className=" text-center ml-8" />
-              <h3 className="font-poppins w-[40%] text-center text-black font-medium ">
-                <input
-                  type="text"
-                  placeholder="Search for a City"
-                  className=" inline-block w-[215px] bg-transparent border-black focus:outline-none"
-                />
-              </h3>
-            </div>
-            <button className=" ml-[159px] w-[239px] h-[50px] border-2 bg-blue-500 text-white rounded-full text-lg hover:bg-transparent hover:font-bold hover:border-black hover:text-black hover:bg-blue-700">
-              Detect Location
-            </button>
+      <div className="relative flex flex-col items-center justify-center min-h-screen p-3 text-center">
+        <h1 className="text-5xl sm:text-5xl font-poppins font-black leading-6 sm:leading-[135px]">
+          Jurident
+        </h1>
+        <h2 className="text-4xl sm:text-[30px] font-poppins font-black leading-[135px]">
+          <span className="text-blue-500">Empowering</span> Justice,
+          <br></br>
+          <span className="text-blue-500">Protecting</span> Rights
+        </h2>
+        <div className="flex flex-col items-center space-y-4 mt-4">
+          <div className="flex items-center text-lg h-16 border-2 border-gray-800 rounded-md">
+            <SlLocationPin className="text-center ml-2 sm:ml-8" />
+            <input
+  type="text"
+  placeholder="Search for a City"
+  className="w-full bg-transparent p-2 border-b border-b-3 border-black focus:outline-none"
+/>
+
           </div>
+          <button className="w-full h-12 border-2 bg-blue-500 text-white rounded-full text-lg hover:bg-transparent hover:font-bold hover:border-black hover:text-black hover:bg-blue-700">
+            Detect Location
+          </button>
         </div>
       </div>
+
     );
   }
 }
