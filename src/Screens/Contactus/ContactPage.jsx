efaa428c54121ea25a585a1f877fc69bf39b1bec
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-12-22 08:54:49 +0530

    fixed contact page

--- src/Screens/Contactus/ContactPage.jsx
+++ src/Screens/Contactus/ContactPage.jsx
@@ -59,7 +59,7 @@ function ContactPage() {
             
           </div>
           
-          <div className="formdiv grid md:grid-cols-2 md:gap-6 sm:grid-cols-1 sm:gap-12" >
+          <div className="formdiv grid md:grid-cols-2 md:gap-6 sm:grid-cols-1 sm:gap-10" >
           <form className="mb-6 group mt-4 mr-14 "style={{ width: "90%" , color:"white", fontSize:"12px"}} onSubmit={handleSubmit}>
           <div>
             <label className="text-black" htmlFor="fName"
@@ -162,7 +162,7 @@ function ContactPage() {
           </div>
           <button type="button" className="text-white bg-yellow-600 hover:bg-yellow-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-700 dark:border-yellow-700">SUBMIT</button>
         </form>
-        <div className="mb-6 group map"style={{width:"40%",height:"60px"}}>
+        <div className="mb-2 group map"style={{width:"40%",height:"60px"}}>
     
             
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119150.55857764366!2d79.07185615537055!3d12.925560220092514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bad38e61fa68ffb%3A0xbedda6917d262b5e!2sVellore%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1688713043344!5m2!1sen!2sin" width="200%" height="800%" style={{ border: "0" ,margin:"20px"}}></iframe>
